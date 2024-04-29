576c7104fbdf6c91c7bdee67e8e72520a1e8f0bb
keatonuw <keatonkowal@icloud.com>
2024-04-29 14:40:11 -0700

    image endpoint

--- app/__init__.py
+++ app/__init__.py
@@ -3,9 +3,15 @@
 
 from flask import Flask
 from flask import render_template
+from flask import send_file
+import io
+from diffusers import StableDiffusionPipeline
 import random
 
 from . import composer
+
+pipe = StableDiffusionPipeline.from_pretrained("cw/")
+pipe.to("mps")  # or CUDA on windows/linux
 
 composer.init()
 print("MODELS LOADED")
@@ -22,6 +28,20 @@ def create_app():
             title=title,
             body=body,
             style=random.choice(["dark", "light", "matrix", "blue"]),
+        )
+
+    @app.route("/image")
+    def image():
+        prompt = "dark, scary, grainy cctv footage l34ks of an urban city street"
+        image = pipe(prompt, num_inference_steps=5, guidance_scale=7.5).images[0]
+        bts = io.BytesIO()
+        image.save(bts, format="jpeg")
+        bts.seek(0)
+        return send_file(
+            bts,
+            mimetype="image/jpeg",
+            download_name="gen.jpg",
+            as_attachment=True,
         )
 
     return app
